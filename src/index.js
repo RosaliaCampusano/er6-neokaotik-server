40d1c5ca9cdccc36effbc5750f6204208df80057
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-28 23:10:36 +0200

    refactor: change the name of the files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
-require("dotenv").config()
-
+require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const mongodbRoute = process.env.MONGODB_CONNECTION_STRING;
-
+const mongodbRoute = process.env.MONGODB_CONNECTION;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-// Use bodyparser 
 app.use(bodyParser.json());
 
-
-async function start(){
-    try{
-        await mongoose.connect(mongodbRoute);
-        app.listen(PORT, () => {
-            console.log(`API is listening on port ${PORT}`);
-        });
-    }catch(error){
-        console.log(`Error al conectar a la base de datos: ${error.message}`);
-    }
+async function start() {
+  try {
+    await mongoose.connect(mongodbRoute);
+    app.listen(PORT, () => {
+      console.log(`API is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 start();
