1dd74ae5124f12f4f7e0eed5c567b5f973526ee1
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-29 22:28:45 +0200

    feat(NK-26): implement the logic to insert new player in the database

--- src/database/Player.js
+++ src/database/Player.js
@@ -1,1 +1,13 @@
+const Player = require("../models/playerModel");
 
+const createPlayer = async (newPlayer) => {
+  try {
+    let playerToInsert = new Player(newPlayer);
+    const createdPlayer = await playerToInsert.save();
+    return createdPlayer;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = { createPlayer };
