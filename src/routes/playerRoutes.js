d5972e811032123bef0100bf4e4daef601e64816
Ignacio <ignayh2000@gmail.com>
2025-09-30 08:40:41 +0200

    feat[NK-23]: Implement POST query

--- src/routes/playerRoutes.js
+++ src/routes/playerRoutes.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const playerController = require("../controllers/playerController");
+const verifyData = require("../middlewares/verifyData");
+
+router.post("/verifyToken", verifyData, playerController.createPlayer);
+
+
 module.exports = router;
