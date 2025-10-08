2f8347da6183b9b04d1e8ecfafdfd75ccdea3fd0
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-08 18:25:07 +0200

    fix(NK-72): remove roles in the gitignore

---
+++ roles/playerRoles.ts
@@ -0,0 +1,23 @@
+
+export const specialEmails = {
+    istvan : 'classcraft.daw2@aeg.eus',
+    villain : 'ozarate@aeg.eus',
+    mortimer: 'oskar.calvo@aeg.eus',
+    acolyte: '%@ikasle.aeg.eus',
+}
+
+export const roles = {
+    'istvan': 'ISTVAN',
+    'villain': 'VILLAIN',
+    'mortimer': 'MORTIMER',
+    'acolyte': 'ACOLYTE',
+
+};
+
+
+export const playerRolesByEmail = {
+    [specialEmails.istvan] : roles.istvan,
+    [specialEmails.villain]: roles.villain,
+    [specialEmails.mortimer]: roles.mortimer,
+    [specialEmails.acolyte]: roles.acolyte,
+};
