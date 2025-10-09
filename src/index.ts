de544825f3a227f5fdce5cc8f22a982980cf9c5f
Ignacio <ignayh2000@gmail.com>
2025-10-09 07:06:02 +0200

    fix(NK-70): Convert firebaseAccount object to firebase service env variables

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,37 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import router from "./routes/playerRoutes";
 
-import firebaseAccount from "../firebase-service.json";
-
 const mongodbRoute : string = process.env.MONGODB_CONNECTION
   ? process.env.MONGODB_CONNECTION
   : "";
+
+  interface frService {
+    type: string,
+    project_id: string,
+    private_key_id: string,
+    private_key: string,
+    client_email: string,
+    client_id: string,
+    auth_uri: string,
+    token_uri: string,
+    auth_provider_x509_cert_url: string,
+    client_x509_cert_url: string,
+    universe_domain: string,
+  }
+
+  const firebaseAccount : any = {  
+    type: process.env.FIREBASE_SERVICE_TYPE,
+    project_id: process.env.FIREBASE_SERVICE_PRJECT_ID,
+    private_key_id: process.env.FIREBASE_SERVICE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_SERVICE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_SERVICE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_SERVICE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_SERVICE_AUTH_URI,
+    token_uri: process.env.FIREBASE_SERVICE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_SERVICE_AUTH_PROVIDER_x509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_SERVICE_CLIENT_x509_CERT_URL,
+    universe_domain: process.env.FIREBASE_SERVICE_UNIVERSE_DOMAIN,
+};
 
 const app = express();
 const PORT : number | string = process.env.PORT || 3000;
