f0b853fcd3aa3ef822936917f2768bf947724db5
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-11 18:44:36 +0200

    feat(NK-56): implement the connection with socket and the server

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,26 @@ import admin from "firebase-admin";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import { firebaseAccount } from "./constants";
+import { SocketEvents } from "./constants";
 import router from "./routes/playerRoutes";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import handlerConnection from "./socket/connection";
 
-const mongodbRoute : string = process.env.MONGODB_CONNECTION
+const mongodbRoute: string = process.env.MONGODB_CONNECTION
   ? process.env.MONGODB_CONNECTION
   : "";
 
-  interface frService {
-    type: string,
-    project_id: string,
-    private_key_id: string,
-    private_key: string,
-    client_email: string,
-    client_id: string,
-    auth_uri: string,
-    token_uri: string,
-    auth_provider_x509_cert_url: string,
-    client_x509_cert_url: string,
-    universe_domain: string,
-  }
-
-  const firebaseAccount : any = {  
-    type: process.env.FIREBASE_SERVICE_TYPE,
-    project_id: process.env.FIREBASE_SERVICE_PRJECT_ID,
-    private_key_id: process.env.FIREBASE_SERVICE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_SERVICE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_SERVICE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_SERVICE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_SERVICE_AUTH_URI,
-    token_uri: process.env.FIREBASE_SERVICE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_SERVICE_AUTH_PROVIDER_x509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_SERVICE_CLIENT_x509_CERT_URL,
-    universe_domain: process.env.FIREBASE_SERVICE_UNIVERSE_DOMAIN,
-};
-
 const app = express();
-const PORT : number | string = process.env.PORT || 3000;
-
-firebaseAccount.private_key = firebaseAccount.private_key.replace(/\\n/g, "\n");
+const PORT: number | string = process.env.PORT || 3000;
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -50,13 +32,15 @@ admin.initializeApp({
   }),
 });
 
+io.on(SocketEvents.CONNECTION, handlerConnection);
+
 app.use(bodyParser.json());
 app.use("/api", router);
 
 async function start() {
   try {
     await mongoose.connect(mongodbRoute);
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
       console.log(`API is listening on port ${PORT}`);
     });
   } catch (error: any) {
